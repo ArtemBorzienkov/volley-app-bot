90c347480c7705f5a78e802a752ed118f2f2117f
artemborzienkov <artemborzienkov@gmail.com>
2023-05-11 15:54:31 +0300

    add-reserv-list

--- src/app.ts
+++ src/app.ts
@@ -11,6 +11,7 @@ import {
   PostingData,
   isDev,
   TIME_FOR_POSTING_UTC,
+  getReplaceMembMsg,
 } from './helpers';
 import { config } from './config';
 
@@ -102,11 +103,33 @@ const removeMembers = (chatId: string, date: string, user: IUser) => {
   }
 
   const training = server.db[chatId][date];
-  if (training.members.length === 0 || !training.members.some((m) => m.id === user.id)) {
-    return;
+  const isUserInMembersList = training.members && training.members.some((m) => m.id === user.id);
+  const isUserInReservList = training.reserve && training.reserve.some((m) => m.id === user.id);
+
+  if (isUserInReservList) {
+    training.reserve = [...training.reserve.filter((m) => m.id !== user.id)];
   }
 
-  training.members = [...training.members.filter((m) => m.id !== user.id)];
+  if (isUserInMembersList) {
+    const usersFromReserv = [];
+
+    training.members = [...training.members.filter((m) => m.id !== user.id)];
+
+    if (training.reserve) {
+      const membsRemainPlacesNumb = training.maxMembers - training.members.length;
+
+      for (let i = 0; i < membsRemainPlacesNumb; i++) {
+        const addedMemb = training.reserve.shift();
+        if (addedMemb) {
+          usersFromReserv.push(addedMemb);
+        }
+      }
+
+      training.members = [...training.members, ...usersFromReserv];
+    }
+
+    server.bot.sendMessage(chatId, getReplaceMembMsg(user, date, usersFromReserv));
+  }
 
   server.bot.editMessageText(getMembersMsg(training.members, training.maxMembers), {
     chat_id: chatId,
@@ -115,12 +138,11 @@ const removeMembers = (chatId: string, date: string, user: IUser) => {
 };
 
 const registryNewMembers = (chatId: string, date: string, user: IUser, value: number) => {
-  console.log('🚀 ~ file: app.ts:119 ~ registryNewMembers ~ user:', user);
   if (!chatId || !date || !user || !server.db[chatId] || !server.db[chatId][date]) {
     return;
   }
 
-  console.log(`add member: ${user}`);
+  console.log('add member:', user);
 
   // TODO: Hardcode for Kostya
   const isUserCoach = user.id === 1115502449;
@@ -141,26 +163,44 @@ const registryNewMembers = (chatId: string, date: string, user: IUser, value: nu
   const training = server.db[chatId][date];
 
   if (training.members) {
+    const membersNumb = training.members.length;
     const isUserAlreadyRegistered = training.members.some((m) => m.id === user.id);
+    const isMembersListFull = membersNumb >= training.maxMembers;
+    const hasEnoughPlaces = !isMembersListFull && membersNumb + value <= training.maxMembers;
+
     if (isUserAlreadyRegistered && !isUserCoach) {
       console.log(`${getUserPrint(user)} is already registered`);
       return;
     }
 
-    if (training.members.length >= training.maxMembers) {
-      console.log('members list is already full');
-      return;
-    }
+    if (hasEnoughPlaces) {
+      console.log('members list is not full, user goes to members list');
+      training.members = [...training.members.filter((m) => m.id !== user.id), ...newMemb];
+    } else if (!isMembersListFull) {
+      console.log('members list is partially full, some users go to members list and some to reserv');
+      const reservUsers = [...newMemb];
+      const memberUsers = [];
+      const remainPlacesNumb = training.maxMembers - membersNumb;
 
-    training.members = [...training.members.filter((m) => m.id !== user.id), ...newMemb];
+      for (let i = 0; i < remainPlacesNumb; i++) {
+        const removed = reservUsers.shift();
+        memberUsers.push(removed);
+      }
+
+      training.members = [...training.members.filter((m) => m.id !== user.id), ...memberUsers];
+      training.reserve = training.reserve ? [...training.reserve.filter((m) => m.id !== user.id), ...reservUsers] : reservUsers;
+    } else {
+      console.log('members list is already full, user goes to reserv list');
+      training.reserve = training.reserve ? [...training.reserve.filter((m) => m.id !== user.id), ...newMemb] : newMemb;
+    }
   } else {
     training.members = newMemb;
   }
 
   if (!training.msg) {
-    server.bot.sendMessage(chatId, getMembersMsg(training.members, training.maxMembers)).then((data) => (training.msg = data.message_id));
+    server.bot.sendMessage(chatId, getMembersMsg(training.members, training.maxMembers, training.reserve)).then((data) => (training.msg = data.message_id));
   } else {
-    server.bot.editMessageText(getMembersMsg(training.members, training.maxMembers), {
+    server.bot.editMessageText(getMembersMsg(training.members, training.maxMembers, training.reserve), {
       chat_id: chatId,
       message_id: training.msg,
     });
